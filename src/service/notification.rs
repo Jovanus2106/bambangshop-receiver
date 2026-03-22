552c2657a1abe141043a65e172b6001cd6b8199f
Jovanus Irwan <jovanusirwan@gmail.com>
2026-03-22 15:44:07 +0700

    Implement receive_notification function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -111,4 +111,8 @@ pub fn unsubscribe(product_type: &str) -> Result<SubscriberRequest> {
         .join()
         .unwrap();
 }
+pub fn receive_notification(payload: Notification) -> Result<Notification> {
+    let subscriber_result: Notification = NotificationRepository::add(payload);
+    return Ok(subscriber_result);
+}
 }
